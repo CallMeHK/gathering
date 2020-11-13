a8aa192cf8049447f482b0822abdeb3c3a80f897
Tyler Gregg <tyler-gregg@pluralsight.com>
2020-11-13 16:53:14 -0700

    deploy

--- deploy.bash
+++ deploy.bash
@@ -1,13 +1,13 @@
 # Initial setup
-$ mix deps.get --only prod
-$ MIX_ENV=prod mix compile
+ mix deps.get --only prod
+ MIX_ENV=prod mix compile
 
 # Compile assets
-$ npm run deploy --prefix ./assets
-$ mix phx.digest
+ npm run deploy --prefix ./assets
+ mix phx.digest
 
 # Custom tasks (like DB migrations)
-$ MIX_ENV=prod mix ecto.migrate
+ MIX_ENV=prod mix ecto.migrate
 
 # Finally run the server
-$ PORT=4001 MIX_ENV=prod mix phx.server
+ PORT=4000 MIX_ENV=prod mix phx.server
